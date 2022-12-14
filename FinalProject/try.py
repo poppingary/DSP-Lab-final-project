b7cbdd754218ae8bd56342bc5126f8831f39b062
KeatonSu <94647428+KeatonSu@users.noreply.github.com>
2022-12-14 00:32:54 -0500

    change tkinter

--- FinalProject/try.py
+++ FinalProject/try.py
@@ -8,9 +8,6 @@ import tkinter as Tk
 import scipy.signal as sps
 
 
-
-
-
 WIDTH = 2
 CHANNELS = 1
 RATE = 16000
@@ -29,47 +26,6 @@ stream = p.open(
     input       = True,
     output      = True )
 
-
-
-# set up vedio
-
-video_capture = cv2.VideoCapture(0)
-
-IS_BRIGHT = True
-Continue = True
-
-
-def isbright(image, dim=10, thresh=100.0):
-    # Resize image to 10x10
-    image = cv2.resize(image, (dim, dim))
-    # Convert color space to LAB format and extract L channel
-    im_hsv = cv2.split(cv2.cvtColor(image, cv2.COLOR_BGR2LAB))
-    # Normalize channel by dividing all pixel values with maximum pixel value
-    v = im_hsv[:][:][0]
-    # Return True if mean is greater than thresh else False
-    return 'light' if np.mean(im_hsv[:][:][0]) > thresh else 'dark'
-
-
-def play_background_music(image,thresh=100):
-    global IS_BRIGHT
-    global thresh_var
-    tsh = thresh_var.get()
-    if isbright(image,thresh=tsh) == 'light' and not IS_BRIGHT:
-        mixer.music.stop()
-        mixer.music.load('light_music2.wav')
-        mixer.music.play()
-        print(isbright(image))
-        IS_BRIGHT = True
-
-    if isbright(image,thresh=tsh) == 'dark' and IS_BRIGHT:
-        mixer.music.stop()
-        mixer.music.load('dark_music2.wav')
-        mixer.music.play()
-        print(isbright(image))
-        IS_BRIGHT = False
-
-
-mixer.init()
 
 
 
@@ -106,6 +62,53 @@ S_thresh = Tk.Scale(root, label = 'thresh', variable = thresh_var, from_ = 0, to
 
 L1.pack(side = Tk.TOP)
 S_thresh.pack(side = Tk.BOTTOM)
+
+
+
+
+
+# set up vedio
+
+video_capture = cv2.VideoCapture(0)
+
+IS_BRIGHT = True
+Continue = True
+
+
+def isbright(image, dim=10, thresh=100.0):
+    # Resize image to 10x10
+    image = cv2.resize(image, (dim, dim))
+    # Convert color space to LAB format and extract L channel
+    im_hsv = cv2.split(cv2.cvtColor(image, cv2.COLOR_BGR2LAB))
+    # Normalize channel by dividing all pixel values with maximum pixel value
+    v = im_hsv[:][:][0]
+    # Return True if mean is greater than thresh else False
+    return 'light' if np.mean(im_hsv[:][:][0]) > thresh else 'dark'
+
+
+def play_background_music(image):
+    global IS_BRIGHT
+    global thresh_var
+    tsh = thresh_var.get()
+    if isbright(image,thresh=tsh) == 'light' and not IS_BRIGHT:
+        mixer.music.stop()
+        mixer.music.load('light_music2.wav')
+        mixer.music.play()
+        print(isbright(image))
+        IS_BRIGHT = True
+
+    if isbright(image,thresh=tsh) == 'dark' and IS_BRIGHT:
+        mixer.music.stop()
+        mixer.music.load('dark_music2.wav')
+        mixer.music.play()
+        print(isbright(image))
+        IS_BRIGHT = False
+
+
+mixer.init()
+
+
+
 
 
 

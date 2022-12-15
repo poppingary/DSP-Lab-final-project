0e997b319b9412bf54cd779a1278db64b5b65ebd
Gary <poppingary@gmail.com>
2022-12-15 09:18:27 -0500

    Add the thread for microphone input.

--- FinalProject/Webcam with microphone.py
+++ FinalProject/Webcam with microphone.py
@@ -7,6 +7,43 @@ import numpy as np
 import pyaudio
 import struct
 
+import _thread
+import time
+
+WIDTH = 2
+CHANNELS = 1
+RATE = 16000
+output_byte = 0
+MAX = 2**15 - 1
+BLOCKLEN = 1200
+output_block = [0] * BLOCKLEN
+
+p = pyaudio.PyAudio()
+# Open audio stream
+stream = p.open(
+    format      = p.get_format_from_width(WIDTH),
+    channels    = CHANNELS,
+    rate        = RATE,
+    input       = True,
+    output      = True)
+
+# Define a function for the thread
+def print_time( threadName, delay):
+   while True:
+       input_tuple = stream.read(BLOCKLEN, exception_on_overflow=False)
+       input_array = struct.unpack('h'*BLOCKLEN, input_tuple)
+
+       # output
+       output_array = input_array
+       output_clip = np.clip(output_array, -MAX, MAX)
+       output_clip = output_clip.astype(int)
+
+       binary_data = struct.pack('h'*BLOCKLEN, *output_clip)
+
+       stream.write(binary_data)
+
+_thread.start_new_thread( print_time, ("Thread-1", 2, ) )
+
 root = Tk()
 root.title('Webcam and background music controller')
 # root.geometry("500x400")
@@ -60,36 +97,11 @@ button = Button(root, image = next_button_image, command = next_song,
 borderwidth = 0)
 
 
-WIDTH = 2
-CHANNELS = 1
-RATE = 16000
-output_byte = 0
-MAX = 2**15 - 1
-BLOCKLEN = 1200
-output_block = [0] * BLOCKLEN
 
-p = pyaudio.PyAudio()
-# Open audio stream
-stream = p.open(
-    format      = p.get_format_from_width(WIDTH),
-    channels    = CHANNELS,
-    rate        = RATE,
-    input       = True,
-    output      = True)
 
-video_capture = cv2.VideoCapture(0)
 
-IS_BRIGHT = True
 
-def isbright(image, dim=10, thresh=100):
-    # Resize image to 10x10
-    image = cv2.resize(image, (dim, dim))
-    # Convert color space to LAB format and extract L channel
-    im_hsv = cv2.split(cv2.cvtColor(image, cv2.COLOR_BGR2LAB))
-    # Normalize channel by dividing all pixel values with maximum pixel value
-    v = im_hsv[:][:][0]
-    # Return True if mean is greater than thresh else False
-    return 'light' if np.mean(v) > thresh else 'dark'
+
 
 # Create Volume Function
 # =============================================================================
@@ -115,12 +127,25 @@ def isbright(image, dim=10, thresh=100):
 # 		volume_meter.config(image=vol4)	
 # =============================================================================
 
+video_capture = cv2.VideoCapture(0)
+IS_BRIGHT = True
+
+def isbright(image, dim=10, thresh=100):
+    # Resize image to 10x10
+    image = cv2.resize(image, (dim, dim))
+    # Convert color space to LAB format and extract L channel
+    im_hsv = cv2.split(cv2.cvtColor(image, cv2.COLOR_BGR2LAB))
+    # Normalize channel by dividing all pixel values with maximum pixel value
+    v = im_hsv[:][:][0]
+    # Return True if mean is greater than thresh else False
+    return 'light' if np.mean(v) > thresh else 'dark'
+
 def play_background_music(image):
     global IS_BRIGHT
     if isbright(image) == 'light' and not IS_BRIGHT:
         pygame.mixer.music.stop()
         pygame.mixer.music.load('light_music2.wav')
-        pygame.mixer.music.set_volume(0.1)
+        pygame.mixer.music.set_volume(0.05)
         pygame.mixer.music.play()
         print(isbright(image))
         IS_BRIGHT = True
@@ -128,12 +153,22 @@ def play_background_music(image):
     if isbright(image) == 'dark' and IS_BRIGHT:
         pygame.mixer.music.stop()
         pygame.mixer.music.load('dark_music2.wav')
-        pygame.mixer.music.set_volume(0.1)
+        pygame.mixer.music.set_volume(0.05)
         pygame.mixer.music.play()
         print(isbright(image))
         IS_BRIGHT = False
 
 def show_frames():
+   ret, frame = video_capture.read()
+
+   # Display the resulting frame
+   # cv2.imshow('camera capture', frame)
+   
+   # Get bright or dark on webcam
+   play_background_music(frame.copy())
+
+   # if cv2.waitKey(1) & 0xFF == ord('q'):
+   #     break
    # Get the latest frame and convert into Image
    cv2image = cv2.cvtColor(video_capture.read()[1], cv2.COLOR_BGR2RGB)
    img = Image.fromarray(cv2image)
@@ -141,53 +176,32 @@ def show_frames():
    imgtk = ImageTk.PhotoImage(image = img)
    webcam_label.imgtk = imgtk
    webcam_label.configure(image = imgtk)
-   
-
-   
-   # Get bright or dark on webcam
-   ret, frame = video_capture.read()
-   play_background_music(frame.copy())
-
-   input_tuple = stream.read(BLOCKLEN, exception_on_overflow=False)
-   input_array = struct.unpack('h'*BLOCKLEN, input_tuple)
-
-   # output
-   output_array = input_array
-   output_clip = np.clip(output_array, -MAX, MAX)
-   output_clip = output_clip.astype(int)
-
-   binary_data = struct.pack('h'*BLOCKLEN, *output_clip)
-
-   stream.write(binary_data)
-   # Repeat after an interval to capture continiously
    webcam_label.after(1, show_frames)
+   
+   
+
+   
+# =============================================================================
+#    # Get bright or dark on webcam
+#    ret, frame = video_capture.read()
+#    play_background_music(frame.copy())
+# 
+#    input_tuple = stream.read(BLOCKLEN, exception_on_overflow=False)
+#    input_array = struct.unpack('h'*BLOCKLEN, input_tuple)
+# 
+#    # output
+#    output_array = input_array
+#    output_clip = np.clip(output_array, -MAX, MAX)
+#    output_clip = output_clip.astype(int)
+# 
+#    binary_data = struct.pack('h'*BLOCKLEN, *output_clip)
+# 
+#    stream.write(binary_data)
+#    # Repeat after an interval to capture continiously
+# =============================================================================
+
 
 show_frames()
-# =============================================================================
-# while True:
-#     ret, frame = video_capture.read()
-# 
-#     # Display the resulting frame
-#     # cv2.imshow('camera capture', frame)
-#     
-#     # Get bright or dark on webcam
-#     play_background_music(frame.copy())
-# 
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-# 
-#     input_tuple = stream.read(BLOCKLEN, exception_on_overflow=False)
-#     input_array = struct.unpack('h'*BLOCKLEN, input_tuple)
-# 
-#     # output
-#     output_array = input_array
-#     output_clip = np.clip(output_array, -MAX, MAX)
-#     output_clip = output_clip.astype(int)
-# 
-#     binary_data = struct.pack('h'*BLOCKLEN, *output_clip)
-# 
-#     stream.write(binary_data)
-# =============================================================================
 
 
 root.mainloop()

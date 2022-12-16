1d20031d59367908faf99d44b061764807e56029
Gary <poppingary@gmail.com>
2022-12-16 06:05:16 -0500

    Add the function of high pass filter.

--- FinalProject/Webcam with microphone.py
+++ FinalProject/Webcam with microphone.py
@@ -7,59 +7,19 @@ import cv2
 import numpy as np
 import pyaudio
 import struct
+from scipy import signal
 
 import _thread
 import time
 
-WIDTH = 2
-CHANNELS = 1
-RATE = 16000
-output_byte = 0
-MAX = 2**15 - 1
-BLOCKLEN = 1200
-output_block = [0] * BLOCKLEN
-
-p = pyaudio.PyAudio()
-# Open audio stream
-stream = p.open(
-    format      = p.get_format_from_width(WIDTH),
-    channels    = CHANNELS,
-    rate        = RATE,
-    input       = True,
-    output      = True)
-
-# Define a function for the thread
-def microphone():
-   while True:
-       input_tuple = stream.read(BLOCKLEN, exception_on_overflow=False)
-       input_array = struct.unpack('h'*BLOCKLEN, input_tuple)
-
-       # output
-       output_array = input_array
-       output_clip = np.clip(output_array, -MAX, MAX)
-       output_clip = output_clip.astype(int)
-
-       binary_data = struct.pack('h'*BLOCKLEN, *output_clip)
-
-       stream.write(binary_data)
-       
-   print('* Finished')
-   stream.stop_stream()
-   stream.close()
-   p.terminate()
-
-_thread.start_new_thread(microphone, ())
-
-
-
 root = Tk()
 root.title('Webcam and background music controller')
-# root.geometry("500x400")
 root.maxsize(1700, 900)  # specify the max size the window can expand to
 root.config(bg="skyblue")  # specify background color
 
 # Initialze Pygame Mixer
 pygame.mixer.init()
+
 
 def volume(x):
     pygame.mixer.music.set_volume(volume_slider.get())
@@ -68,6 +28,11 @@ THRESH = 100
 def brightness_thresh(x):
     global THRESH
     THRESH = brightness_thresh_slider.get()
+    
+HIGH_PASS_FREQ = 750
+def high_pass(x):
+    global HIGH_PASS_FREQ
+    HIGH_PASS_FREQ = high_pass_slider.get()
     
 def next_song(x):
 	print("next song")
@@ -82,11 +47,11 @@ right_frame.grid(row=0, column=1, padx=10, pady=5)
 # Create frames and labels in left_frame
 Label(left_frame, text="Background music").grid(row=0, column=0, padx=5, pady=5)
 
-# load image to be "edited"
+# load music image
 small_image = ImageTk.PhotoImage(Image.open('images/hello_world.png').resize((180, 180)))
 Label(left_frame, image = small_image).grid(row = 1, column = 0, padx = 5, pady = 5)
 
-# Display image in right_frame
+# Webcam is in right_frame
 large_image = ImageTk.PhotoImage(Image.open('images/pikachu.png').resize((940, 770)))
 webcam_label = Label(right_frame, image = large_image)
 webcam_label.grid(row = 0,column = 0, padx = 5, pady = 5)
@@ -96,19 +61,18 @@ tool_bar = Frame(left_frame, width = 200, height = 185)
 tool_bar.grid(row = 2, column = 0, padx = 5, pady = 5)
 
 # Create volume label frame
-volume_frame = LabelFrame(tool_bar, text="Volume")
+volume_frame = LabelFrame(tool_bar, text = "Volume")
 volume_frame.grid(row = 0 , column = 0)
 
 # Create brightness thresh label frame
-brightness_thresh_frame = LabelFrame(tool_bar, text="Brightness thresh")
+brightness_thresh_frame = LabelFrame(tool_bar, text = "Brightness thresh")
 brightness_thresh_frame.grid(row = 1 , column = 0)
 
-# Example labels that serve as placeholders for other widgets
-# =============================================================================
-# Label(tool_bar, text="Tools", relief=RAISED).grid(row=0, column=0, padx=5, pady=3, ipadx=10)  # ipadx is padding inside the Label widget
-# Label(tool_bar, text="Filters", relief=RAISED).grid(row=0, column=1, padx=5, pady=3, ipadx=10)
-# =============================================================================
+# Create high pass filter label frame
+high_pass_frame = LabelFrame(tool_bar, text = "High-pass filter")
+high_pass_frame.grid(row = 2 , column = 0)
 
+# Create next song button image
 next_button_image = ImageTk.PhotoImage(Image.open('images/next_btn.png').resize((100, 40)))
 
 # Example labels that could be displayed under the "Tool" menu
@@ -116,14 +80,9 @@ volume_slider = ttk.Scale(volume_frame, from_ = 0, to = 1, orient = HORIZONTAL, 
 volume_slider.pack(pady = 10)
 brightness_thresh_slider = ttk.Scale(brightness_thresh_frame, from_ = 0, to = 255, orient = HORIZONTAL, value = 100, command = brightness_thresh, length = 200)
 brightness_thresh_slider.pack(pady = 10)
-# =============================================================================
-# Label(tool_bar, text="Select").grid(row=1, column=0, padx=5, pady=5)
-# Label(tool_bar, text="Crop").grid(row=2, column=0, padx=5, pady=5)
-# Label(tool_bar, text="Rotate & Flip").grid(row=3, column=0, padx=5, pady=5)
-# Label(tool_bar, text="Resize").grid(row=4, column=0, padx=5, pady=5)
-# Label(tool_bar, text="Exposure").grid(row=5, column=0, padx=5, pady=5)
-# =============================================================================
-Label(tool_bar, image=next_button_image).grid(row = 3, column = 0, padx = 5, pady = 5, columnspan = 2)
+high_pass_slider = ttk.Scale(high_pass_frame, from_ = 10, to = 1500, orient = HORIZONTAL, value = 750, command = high_pass, length = 200)
+high_pass_slider.pack(pady = 10)
+Label(tool_bar, image = next_button_image).grid(row = 3, column = 0, padx = 5, pady = 5, columnspan = 2)
 button = Button(root, image = next_button_image, command = next_song, borderwidth = 0)
 
 video_capture = cv2.VideoCapture(0)
@@ -143,7 +102,7 @@ def play_background_music(image):
     global IS_BRIGHT
     if isbright(image) == 'light' and not IS_BRIGHT:
         pygame.mixer.music.stop()
-        pygame.mixer.music.load('light_music2.wav')
+        pygame.mixer.music.load('light_music.wav')
         pygame.mixer.music.set_volume(0.2)
         pygame.mixer.music.play()
         print(isbright(image))
@@ -151,7 +110,7 @@ def play_background_music(image):
 
     if isbright(image) == 'dark' and IS_BRIGHT:
         pygame.mixer.music.stop()
-        pygame.mixer.music.load('dark_music2.wav')
+        pygame.mixer.music.load('quiet_music1.wav')
         pygame.mixer.music.set_volume(0.2)
         pygame.mixer.music.play()
         print(isbright(image))
@@ -178,6 +137,55 @@ def show_frames():
    webcam_label.after(1, show_frames)
 
 show_frames()
+
+
+def microphone():
+   global states
+   global HIGH_PASS_FREQ
+   WIDTH = 2
+   CHANNELS = 1
+   RATE = 16000
+   output_byte = 0
+   MAX = 2**15 - 1
+   BLOCKLEN = 1200
+   output_block = [0] * BLOCKLEN
+   ORDER = 5  # order of filter
+   states = [0] * ORDER   # initial states
+
+   p = pyaudio.PyAudio()
+   # Open audio stream
+   stream = p.open(
+       format      = p.get_format_from_width(WIDTH),
+       channels    = CHANNELS,
+       rate        = RATE,
+       input       = True,
+       output      = True)
+
+   while True:
+       input_tuple = stream.read(BLOCKLEN, exception_on_overflow = False)
+       input_array = struct.unpack('h'*BLOCKLEN, input_tuple)
+
+       b, a = signal.butter(ORDER, HIGH_PASS_FREQ / (RATE / 2) , btype = 'highpass')   # apply highpass filter for bright space
+       [filter_signal, states] = signal.lfilter(b, a, input_array, zi = states)
+
+       # output
+       output_array = filter_signal
+       output_clip = np.clip(output_array, -MAX, MAX)
+       output_clip = output_clip.astype(int)
+
+       binary_data = struct.pack('h'*BLOCKLEN, *output_clip)
+
+       stream.write(binary_data)
+       
+   print('* Finished')
+   stream.stop_stream()
+   stream.close()
+   p.terminate()
+   
+MICROPHONE_THREAD_COUNTER = 0
+if MICROPHONE_THREAD_COUNTER == 0:
+    _thread.start_new_thread(microphone, ())
+    MICROPHONE_THREAD_COUNTER += 1
 
 
 root.mainloop()
